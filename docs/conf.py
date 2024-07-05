9c9ff233bd058e5d82f58b455da1d388605205ba
Pan Ei Phyo <pan@brycenmyanmar.com.mm>
2024-07-05 12:03:18 +0630

    add conf.py

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+# docsconf.py
+
+# -- Project information -----------------------------------------------------
+
+project = 'Your Project Name'
+html_title = 'Your Project Documentation'
+
+# -- General configuration ---------------------------------------------------
+
+# Add extensions here if needed
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
